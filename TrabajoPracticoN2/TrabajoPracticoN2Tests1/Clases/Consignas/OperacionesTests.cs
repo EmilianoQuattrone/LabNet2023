bbfc47682738cbb86d1e4150bbd961368b75779a
Emiliano Quattrone <emilianoquattrone008mail.com>
2023-03-07 22:48:34 -0300

    Octavo Commit TP2

---
+++ TrabajoPracticoN2/TrabajoPracticoN2Tests1/Clases/Consignas/OperacionesTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrabajoPracticoN2.Clases.Consignas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPracticoN2.Clases.Consignas.Tests
+{
+    [TestClass()]
+    public class OperacionesTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivisionEJN1Test()
+        {
+            decimal dividendo = 10;
+
+            Operaciones.DivisionEJN1(dividendo);
+        }
+    }
+}
