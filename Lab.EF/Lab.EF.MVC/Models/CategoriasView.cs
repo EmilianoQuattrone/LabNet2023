e38e94845e707a329e9195d612a541a8e5a568ab
Emiliano Quattrone <emilianoquattrone008mail.com>
2023-03-22 10:39:47 -0300

    Commit N9 validaciones de numeros en los inpunts

--- Lab.EF/Lab.EF.MVC/Models/CategoriasView.cs
+++ Lab.EF/Lab.EF.MVC/Models/CategoriasView.cs
@@ -11,8 +11,10 @@ namespace Lab.EF.MVC.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El nombre es requerido")]
+        [RegularExpression("^[^0-9]+$", ErrorMessage = "El nombre de categorias no acepta valores numericos")]
         [StringLength(15)]
         public string Nombre { get; set; }
+        [RegularExpression("^[^0-9]+$", ErrorMessage = "La descripcion no acepta valores numericos")]
         public string Descripcion { get; set; }
     }
 }
