4c422e27346ddba10f7262bdf2b258880f74dbcf
Emiliano Quattrone <emilianoquattrone008mail.com>
2023-03-23 22:43:15 -0300

    Commit N1 Api publica

---
+++ Lab.EF/Lab.EF.MVC.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Lab.EF.MVC.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Lab.EF/Lab.EF.MVC.API/Controllers/ApiPublicaController.cs
@@ -0,0 +1,26 @@
+﻿using Lab.EF.MVC.API.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.EF.MVC.API.Controllers
+{
+    public class ApiPublicaController : Controller
+    {
+        public string url = "https://jsonplaceholder.typicode.com/users";
+        // GET: ApiPublica
+        public async Task<ActionResult> ConsumirApiPublica()
+        {
+            var httpClient = new HttpClient();
+            //json de momento devuelve un string por el metodo
+            var respuesta = await httpClient.GetStringAsync(url);
+            var modelo = JsonConvert.DeserializeObject<UsuariosView[]>(respuesta);
+            return View(modelo);
+        }
+    }
+}
